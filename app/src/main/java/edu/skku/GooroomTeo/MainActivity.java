619a8fd56c585131759b3ecd9c983eb2d43118b4
hursung1 <hwan4216@gmail.com>
2019-05-28 22:20:50 +0900

    Test for git upload

--- app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
+++ app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button addbtn;
+    Button findbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
         addbtn = findViewById(R.id.addcontent);
         addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findbtn = findViewById(R.id.findnear);
+        findbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 

bcebd3948b71f4c36bd8933a2d640c38d4cd7e94
hursung1 <hwan4216@gmail.com>
2019-06-02 16:24:11 +0900

    RateActivity basic design completed

--- app/src/main/java/edu/skku/GooroomTeo/RateActivity.java
+++ app/src/main/java/edu/skku/GooroomTeo/RateActivity.java
@@ -5,9 +5,11 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,7 +21,7 @@ import java.util.ArrayList;
 
 public class RateActivity extends AppCompatActivity {
 
-    Button alertwrongbtn;
+    Button alertwrongbtn, ratesendbtn;
     ListView listView;
 
     ArrayList<String> data;
@@ -31,6 +33,7 @@ public class RateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rate);
 
         alertwrongbtn = findViewById(R.id.alertwrongbtn);
+        ratesendbtn = findViewById(R.id.ratesendbtn);
         listView = findViewById(R.id.listview);
 
 
@@ -64,6 +67,54 @@ public class RateActivity extends AppCompatActivity {
 
                 AlertDialog alert = alert_body.create();
                 alert.setTitle("잘못된 정보 신고");
+                alert.show();
+            }
+        });
+
+        ratesendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert_body = new AlertDialog.Builder(RateActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialog_view = inflater.inflate(R.layout.dialog_rate, null);
+                alert_body.setView(dialog_view);
+
+
+                final Button sendbtn = view.findViewById(R.id.sendbtn);
+                final Button cancelbtn = view.findViewById(R.id.cancelbtn);
+
+                final EditText rateet = dialog_view .findViewById(R.id.rateet);
+                final EditText commentet = dialog_view .findViewById(R.id.commentet);
+
+                final AlertDialog alert = alert_body.create();
+
+                alert_body.setPositiveButton("보내기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Send message to server
+                        String comment = commentet.getText().toString();
+                        if(comment.length() <= 0){
+                            Toast.makeText(RateActivity.this, "코멘트를 입력하십시오", Toast.LENGTH_LONG).show();
+                        }
+                        else{ // Send info to Server
+
+                        }
+
+
+                        alert.dismiss();
+                    }
+                });
+                alert_body.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Cancel
+                        Toast.makeText(RateActivity.this, "평가 취소하셨습니다.", Toast.LENGTH_LONG).show();
+                        alert.dismiss();
+
+                    }
+                });
+
+                alert.setTitle("이 장소 평가");
                 alert.show();
             }
         });

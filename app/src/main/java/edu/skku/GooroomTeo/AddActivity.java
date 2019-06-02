22acb8fe26a8a3550cad0e5b6498df862325983c
hursung1 <hwan4216@gmail.com>
2019-06-02 10:10:51 +0900

    Little bit modified

--- app/src/main/java/edu/skku/GooroomTeo/AddActivity.java
+++ app/src/main/java/edu/skku/GooroomTeo/AddActivity.java
@@ -2,12 +2,34 @@ package edu.skku.GooroomTeo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddActivity extends AppCompatActivity {
+    Button uploadbtn, savebtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        uploadbtn = findViewById(R.id.uploadbtn);
+        savebtn = findViewById(R.id.savebtn);
+
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
+++ app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.skku.GooroomTeo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         addbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
             }
         });
 

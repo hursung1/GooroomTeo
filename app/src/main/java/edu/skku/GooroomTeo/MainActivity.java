d999c355244eecd0bcc5cb4be601e8e0650201e4
jungsunwoung <94wjdtjsdnd@naver.com>
2019-06-04 21:51:11 +0900

    working

--- app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
+++ app/src/main/java/edu/skku/GooroomTeo/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +16,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.app.FragmentManager;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,21 +24,46 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity
-        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     Button addbtn;
     Button findbtn;
     GoogleMap map;
-    double longitude;
-    double latitude;
+    double lon;
+    double lat;
+    double comlon;
+    double comlat;
+    double shortlon;
+    double shortlat;
+    String [] splitstring;
+    String getFromFire;
+    double shortest;
+    String shortestPlace;
+    private DatabaseReference mPostReference;
+    ArrayList<String> data;
+    double [] testarray;
+    int a;
+    String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager
+        data = new ArrayList<String>();
+        MapFragment mapFragment = (MapFragment) fragmentManager
+
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -57,6 +82,27 @@ public class MainActivity extends AppCompatActivity
                     0,
                     networkLocationListener);
         }
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+
+        Log.d("before mark", "tag" );
+        //지도에 마커표시하기
+        getFirebaseDatabase();
+        for (int i=0; i<data.size(); i++){
+            getFromFire=data.get(i);
+            splitstring=getFromFire.split(":");
+            comlon=Double.valueOf(splitstring[0]).doubleValue();
+            comlat=Double.valueOf(splitstring[1]).doubleValue();
+            title=splitstring[2];
+            LatLng marker=new LatLng(comlat,comlon);
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(marker);
+            markerOptions.title(title);
+            map.addMarker(markerOptions);
+
+
+        }
+
+        Log.d("after mark", "tag" );
 
         addbtn = findViewById(R.id.addcontent);
         addbtn.setOnClickListener(new View.OnClickListener() {
@@ -67,12 +113,14 @@ public class MainActivity extends AppCompatActivity
             }
 
         });
-
         findbtn = findViewById(R.id.findnear);
         findbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LatLng MyPos = new LatLng(latitude, longitude);
+                LatLng MyPos = new LatLng(lat, lon);
+                Location current=new Location("myCurrent");
+                current.setLatitude(lat);
+                current.setLongitude(lon);
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions.position(MyPos);
                 markerOptions.title("현위치");
@@ -80,25 +128,59 @@ public class MainActivity extends AppCompatActivity
                 map.moveCamera(CameraUpdateFactory.newLatLng(MyPos));
                 map.animateCamera(CameraUpdateFactory.zoomTo(16));
 
+                //데이터 받아와서 거리계산하기
+                getFirebaseDatabase();
+                shortest=100000000;
+                for (int i=0; i<data.size(); i++){
+                    getFromFire=data.get(i);
+                    splitstring=getFromFire.split(":");
+                    comlon=Double.valueOf(splitstring[0]).doubleValue();
+                    comlat=Double.valueOf(splitstring[1]).doubleValue();
+                    Location comPos=new Location("comPos");
+                    comPos.setLatitude(comlat);
+                    comPos.setLongitude(comlon);
+
+                    double distance=current.distanceTo(comPos);
+                    if (shortest>distance){
+                        double temp=distance;
+                        shortest=temp;
+                        a=i;
+                    }
+                }
+                shortestPlace=data.get(a);
+                splitstring=shortestPlace.split(":");
+                shortlat=Double.valueOf(splitstring[1]).doubleValue();
+                shortlon=Double.valueOf(splitstring[0]).doubleValue();
+                LatLng Shortplace = new LatLng(lat, lon);
+                map.moveCamera(CameraUpdateFactory.newLatLng(Shortplace));
+                map.animateCamera(CameraUpdateFactory.zoomTo(16));
+
             }
         });
 
     }
+
     final LocationListener networkLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
             String provider = location.getProvider();
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-            Log.d("Update Network", "" + provider + " " + longitude + " " + latitude);
+            lon = location.getLongitude();
+            lat = location.getLatitude();
+            Log.d("Update Network", "" + provider + " " + lon + " " + lat);
 
         }
+
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {}
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
         @Override
-        public void onProviderEnabled(String provider) {}
+        public void onProviderEnabled(String provider) {
+        }
+
         @Override
-        public void onProviderDisabled(String provider) {}
+        public void onProviderDisabled(String provider) {
+        }
     };
 
 
@@ -116,14 +198,39 @@ public class MainActivity extends AppCompatActivity
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 
-    public boolean onMarkerClick(Marker marker){
+    public boolean onMarkerClick(Marker marker) {
 
         Intent intent = new Intent(MainActivity.this, RateActivity.class);
-        String title=marker.getTitle();
-        intent.putExtra("information",title);
+        String title = marker.getTitle();
+        intent.putExtra("information", title);
         startActivity(intent);
 
         return true;
+
+    }
+
+
+    public void getFirebaseDatabase() {
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("onDataChange", "Data is Updated");
+                data.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    FirebasePost get = postSnapshot.getValue(FirebasePost.class);
+                    String[] info = {String.valueOf(get.longitude), String.valueOf(get.latitude), get.name};
+                    String result = info[0] + " : " + info[1] + " : " + info[2];
+                    data.add(result);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mPostReference.child("locinfo").addValueEventListener(postListener);
 
     }
 }

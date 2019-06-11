5235587cbcfb3f3de6dddb5e0b46dcddb51db904
hursung1 <hwan4216@gmail.com>
2019-06-11 21:18:44 +0900

    Glide added

---
+++ app/src/main/java/edu/skku/GooroomTeo/MyAppGlideModule.java
@@ -0,0 +1,16 @@
+package edu.skku.GooroomTeo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        super.applyOptions(context, builder);
+    }
+}
